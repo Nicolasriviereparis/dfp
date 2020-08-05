36db2d69d1a32773cf281b941e1a58b62792bc9f
Nicolas <nr.riviere@gmail.com>
2020-08-05 14:43:05 +0200

    full update

--- larousse/sw-registration.js
+++
@@ -1,1 +0,0 @@
-import { Workbox } from "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-window.prod.mjs";if ("serviceWorker" in navigator) {const wb = new Workbox("sw.js");wb.register({scope: './',});}
