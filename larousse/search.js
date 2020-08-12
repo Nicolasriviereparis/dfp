4e562a813c61e29dfed5f844b7cd076fe1689d56
Nicolas <nr.riviere@gmail.com>
2020-08-12 14:21:29 +0200

    update

--- larousse/search.js
+++ larousse/search.js
@@ -165,4 +165,33 @@ function showResult() {
       $('body').addClass('scroll-down');
     }
   }
+  // Prompt install PWA
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  if(deferredPrompt != null){
+    console.log(deferredPrompt);
+    $('#installPromptModal').removeClass('hidden');
+    document.querySelector('.btn-close').addEventListener('click', () =>{
+      $('#installPromptModal').addClass('hidden');
+    });
+  }
+});
+
+let buttonInstall = document.querySelector('#install-btn');
+
+buttonInstall.addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  // hideMyInstallPromotion();
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+    // if (choiceResult.outcome === 'accepted') {
+    // } else {
+    // }
+  });
+});
+
 }
